7b2fb70ef29783b5a2c5e12cc6b16f864b7f9b3a
mrida <mrida@tamweely.com.eg>
2025-09-18 12:09:37 +0300

    Implement Interpreter pattern with Terminal, And, and Or expressions; add example usage

---
+++ bin/design_patterns/Behavioral/interpreter.dart
@@ -0,0 +1,64 @@
+/*
+Pattern using :  
+Representation grammar for a language (music m Formulas , ,mathematical expressions, etc.).
+Domain specific languages like SQL, Regex, etc.
+*/
+
+import 'package:string_tokenizer_1/string_tokenizer_1.dart';
+
+abstract class Expression {
+  bool interpret();
+}
+
+class TerminalExpression implements Expression {
+  final String data;
+
+  TerminalExpression(this.data);
+
+  @override
+  bool interpret() {
+    StringTokenizer tokens = StringTokenizer(data);
+    while (tokens.hasMoreTokens()) {
+      final String test = tokens.nextToken();
+      if (test == data) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+class AndExpression implements Expression {
+  final Expression expr1;
+  final Expression expr2;
+
+  AndExpression(this.expr1, this.expr2);
+
+  @override
+  bool interpret() {
+    return expr1.interpret() && expr2.interpret();
+  }
+}
+
+class OrExpression implements Expression {
+  final Expression expr1;
+  final Expression expr2;
+
+  OrExpression(this.expr1, this.expr2);
+
+  @override
+  bool interpret() {
+    return expr1.interpret() || expr2.interpret();
+  }
+}
+
+void main() {
+  // Example usage:
+  Expression expr1 = TerminalExpression('A');
+  Expression expr2 = TerminalExpression('B');
+  Expression andExpr = AndExpression(expr1, expr2);
+  Expression orExpr = OrExpression(expr1, expr2);
+
+  print('AND Expression Result: ${andExpr.interpret()}');
+  print('OR Expression Result: ${orExpr.interpret()}');
+}
